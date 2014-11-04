4ee9e34a000ea86f11a8e8faf2749c2e786af416
Sterling Burrito <Sterling@Sterlings-MBP.home>
2014-11-03 21:26:36 -0500

    added Angular

---
+++ js/app.js
@@ -0,0 +1,86 @@
+/**
+ * Created by Sterling on 10/30/14.
+ */
+
+
+var app = angular.module('portfolio', ['ngRoute']);
+
+
+
+// Routing
+app.config(function ($routeProvider) {
+    $routeProvider
+        .when('/', {
+            templateUrl: 'views/main.html',
+            controller: 'MainCtrl'
+        })
+        .when('/pattaya', {
+            templateUrl: 'views/pattaya.html',
+            controller: 'PortfolioCtrl'
+        })
+        .when('/random-quote', {
+            templateUrl: 'views/random-quote.html',
+            controller: 'PortfolioCtrl'
+        })
+        .otherwise({
+            redirectTo: '/'
+        });
+});
+
+
+
+function inits () {
+
+    // Stellar.js
+    $(function(){
+        $.stellar({
+            horizontalScrolling: false
+        });
+    });
+
+
+    // Wow.js
+    wow = new WOW({
+        animateClass: 'animated',
+        offset:       100,
+        mobile:       true
+    });
+    wow.init();
+
+
+    // Slick Slider
+    $(document).ready(function(){
+        $('.slides').slick({
+            autoplay: true
+        });
+    });
+
+
+    // Scroll
+    $('a').click(function(){
+        $('html, body').animate({
+            scrollTop: $( $.attr(this, 'href') ).offset().top
+        }, 500);
+        return false;
+    });
+}
+
+
+
+
+app.controller('MainCtrl', function ($scope) {
+
+    console.log("YO");
+
+    inits();
+
+});
+
+
+app.controller('PortfolioCtrl', function ($scope) {
+
+    console.log("YO YO YO");
+
+    inits();
+
+});
